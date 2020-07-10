e8a6430400bb6adb06a6e115cae187df3263ceac
guoza <guoza@gzclearing.com>
2020-07-10 16:16:26 +0800

    项目初始化

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="123"/>
+    <div>
+      这是我新创建的vue项目
+    </div>
+    <div>
+      这是首页，可以是登录页，也可以是商城的首页
+    </div>
   </div>
 </template>
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,18 @@
 module.exports = {
-    // 选项...
+  devServer: {
+    proxy: {
+     '/hrm/api': {
+        //target: 'http://192.168.1.209:10751/', // Dev环境
+        //  target: 'http://192.168.1.238:10751/', // Test环境
+        // target: 'http://192.168.1.215:10751/', // Rls环境
+        target: 'http://192.168.1.218:10751/', // 正式环境
+        changeOrigin: true,
+        autoRewrite: true,
+        cookieDomainRewrite: true,
+        pathRewrite: {
+          '^/hrm/api/': '/'
+        }
+      }
+    }
   }
+}
